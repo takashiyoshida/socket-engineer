bd08b9f91ac97b97e9c6184aa0a45771ef914211
Takashi Yoshida <takashi@takashiyoshida.org>
2017-01-17 17:44:54 +0800

    Initial Commit

---
+++ socket-engineer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  socket-engineer
+//
+//  Created by Takashi Yoshida on 1/17/17.
+//  Copyright © 2017 Takashi Yoshida. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
